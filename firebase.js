385026244d5ef556abeed7037011785bfda768c8
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-10-27 22:26:15 +1100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByvHyTPvwSD5iI9gFMeKk89eCoL3P__Ss",
-  authDomain: "reactnative-push-notific-e871b.firebaseapp.com",
-  projectId: "reactnative-push-notific-e871b",
-  storageBucket: "reactnative-push-notific-e871b.appspot.com",
-  messagingSenderId: "98427882719",
-  appId: "1:98427882719:web:891247ae88d46914eaed6a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
