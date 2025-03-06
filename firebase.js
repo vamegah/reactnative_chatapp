e7c48c8a214f798408e0bbf8b24094f633cd4593
vamegah <makvim@yahoo.com>
2025-03-06 00:59:46 -0500

    Add config file

--- firebase.js
+++ firebase.js
@@ -6,14 +6,14 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBatU158y3hogMT1Qyq2Zq3c8OJwHelK8w",
+  authDomain: "reactnative-push-notific-ee83b.firebaseapp.com",
+  projectId: "reactnative-push-notific-ee83b",
+  storageBucket: "reactnative-push-notific-ee83b.firebasestorage.app",
+  messagingSenderId: "128732541059",
+  appId: "1:128732541059:web:13715654ae106a6a696bb6",
+  measurementId: "G-5ZV5L03PKL"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
